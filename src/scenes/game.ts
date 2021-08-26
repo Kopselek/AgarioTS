3ee321260bfa28efae347ea5ba79c63759393724
Kopselek <konrad.kkacperek@interia.pl>
2021-08-26 19:40:39 +0200

    frame independent game

--- src/scenes/game.ts
+++ src/scenes/game.ts
@@ -25,21 +25,19 @@ export class Game extends GameScene{
         this.setCameraZoom(this.cameras.main, 1);
     }
 
-    update ()
+    update (time: number, deltaTime: number)
     {
         const gameScene : GameScene = this;
-        // variable to change if you like
-        const range = 25;
     
-        var size = gameScene.player.scale * 5;
-        var speed = 1 - size / 10;
+        const size = gameScene.player.scale * 5;
+        let speed = 1 - size / 10;
     
-        var mouseX = gameScene.input.mousePointer.x;
-        var mouseY = gameScene.input.mousePointer.y;
-        var centerX = gameScene.cameras.main.centerX;
-        var centerY = gameScene.cameras.main.centerY;
+        const mouseX = gameScene.input.mousePointer.x;
+        const mouseY = gameScene.input.mousePointer.y;
+        const centerX = gameScene.cameras.main.centerX;
+        const centerY = gameScene.cameras.main.centerY;
     
-        var mouseDistance = Phaser.Math.Distance.Between(mouseX, mouseY, centerX, centerY);
+        const mouseDistance = Phaser.Math.Distance.Between(mouseX, mouseY, centerX, centerY);
         if(mouseDistance < 100){
             var removeSpeed = 0.5 - mouseDistance / 200;
             speed = speed - removeSpeed
@@ -47,7 +45,7 @@ export class Game extends GameScene{
     
         let direction = new Phaser.Math.Vector2(mouseX - centerX, mouseY - centerY).normalize();
 
-        gameScene.player.x += direction.x * speed;
-        gameScene.player.y += direction.y * speed;
+        gameScene.player.x += direction.x * speed * deltaTime / 6;
+        gameScene.player.y += direction.y * speed * deltaTime / 6;
     }
 }
