a8b4f55ae0d124ca08c07eb403742a8b46a5d9d8
Kopselek <konrad.kkacperek@interia.pl>
2021-09-02 14:01:23 +0200

    change name of velocity in Ball constructor

--- src/ball.ts
+++ src/ball.ts
@@ -1,13 +1,13 @@
 import { Entity } from "./entity";
 
 export class Ball extends Entity{
-    constructor(scene: Phaser.Scene, x: number, y: number, vector?: Phaser.Math.Vector2){
+    constructor(scene: Phaser.Scene, x: number, y: number, moveTo?: Phaser.Math.Vector2){
         super(scene, x, y);
         this.tint = Math.random() * 0xffffff;
         this.setScale(0.05);
         this.setCircle(80);
         this.setOffset(50,50);
-        if(vector){ this.moveTo(vector, 5, 200) }
+        if(moveTo){ this.moveTo(moveTo, 5, 200) }
     }
 
     public moveTo(vec: Phaser.Math.Vector2, speed: number, time: number){
